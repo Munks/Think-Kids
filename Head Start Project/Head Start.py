339c48edcdff08bab50a2b180cd9a753a524f0ad
Christopher Eddy <christopher.eddy22@gmail.com>
2018-02-26 09:20:42 -0500

    Updates

--- Head Start Project/Head Start.py
+++ Head Start Project/Head Start.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import datetime as dt
 import sys
 sys.path.append(r'C:\Python Programs\Think-Kids_Private')
@@ -31,11 +30,119 @@ file = r'\Head Start Data Raw Data.xlsx'
 df = pd.read_excel(path + file)
 df = df.replace({99: np.nan})
 
-data = df['DEM_AGE']
-plt.hist(data.dropna())
-plt.xlabel('Age')
-plt.ylabel('Count')
-plt.show()
+# Make the CPS-AIM DataFrames
+cps_aim_t1 = ['ID #']
+cps_aim_t2 = ['ID #']
+cps_aim_t3 = ['ID #']
+
+df_time_columns('T1_TKCO', cps_aim_t1)
+df_time_columns('T2_TKCO', cps_aim_t2)
+df_time_columns('T3_TKCO', cps_aim_t3)
+
+cps_aim_df_t1 = df[cps_aim_t1]
+column_names = []
+split_dfs(cps_aim_df_t1, 3, 8)
+cps_aim_df_t1.columns = column_names
+
+cps_aim_df_t2 = df[cps_aim_t2]
+column_names = []
+split_dfs(cps_aim_df_t2, 3, 8)
+cps_aim_df_t2.columns = column_names
+
+cps_aim_df_t3 = df[cps_aim_t3]
+column_names = []
+split_dfs(cps_aim_df_t3, 3, 8)
+cps_aim_df_t3.columns = column_names
+
+cps_aim_t1.remove('ID #')
+cps_aim_t2.remove('ID #')
+cps_aim_t3.remove('ID #')
+all_cps_aim = []
+all_cps_aim.append(cps_aim_t1)
+all_cps_aim.append(cps_aim_t2)
+all_cps_aim.append(cps_aim_t3)
+for i in all_cps_aim:
+    df.drop(i, axis=1, inplace=True)
+
+# Adding the CPS-AIM to the main DataFrame
+
+results = []
+results = score.cps_aim_parent(cps_aim_df_t1, results, '#_', 't1')
+df = pd.merge(df, results, how='left', left_on='ID #', right_on='id')
+df.drop('id', axis=1, inplace=True)
+results = []
+results = score.cps_aim_parent(cps_aim_df_t2, results, '#_', 't2')
+df = pd.merge(df, results, how='left', left_on='ID #', right_on='id')
+df.drop('id', axis=1, inplace=True)
+results = []
+results = score.cps_aim_parent(cps_aim_df_t3, results, '#_', 't3')
+df = pd.merge(df, results, how='left', left_on='ID #', right_on='id')
+df.drop('id', axis=1, inplace=True)
+
+# Make the DERS DataFrames
+
+# DERS is missing items 24 and 36
+ders_t1 = ['ID #']
+ders_t2 = ['ID #']
+ders_t3 = ['ID #']
+
+df_time_columns('T1_DERS', ders_t1)
+df_time_columns('T2_DERS', ders_t2)
+df_time_columns('T3_DERS', ders_t3)
+
+ders_df_t1 = df[ders_t1]
+column_names = []
+split_dfs(ders_df_t1, 3, 7)
+ders_col_names = ['#_']
+for i in range(len(column_names)):
+    if i != 23:
+        ders_col_names.append('ders_' + str(i + 1))
+ders_df_t1.columns = ders_col_names
+
+ders_df_t2 = df[ders_t2]
+column_names = []
+split_dfs(ders_df_t2, 3, 7)
+ders_col_names = ['#_']
+for i in range(len(column_names)):
+    if i != 23:
+        ders_col_names.append('ders_' + str(i + 1))
+ders_df_t2.columns = ders_col_names
+
+ders_df_t3 = df[ders_t3]
+column_names = []
+split_dfs(ders_df_t3, 3, 7)
+ders_col_names = ['#_']
+for i in range(len(column_names)):
+    if i != 23:
+        ders_col_names.append('ders_' + str(i + 1))
+ders_df_t3.columns = ders_col_names
+
+
+ders_t1.remove('ID #')
+ders_t2.remove('ID #')
+ders_t3.remove('ID #')
+all_ders = []
+all_ders.append(ders_t1)
+all_ders.append(ders_t2)
+all_ders.append(ders_t3)
+for i in all_ders:
+    df.drop(i, axis=1, inplace=True)
+
+# Adding the DERS to the main DataFrame
+
+results = []
+results = score.ders(ders_df_t1, results, '#_', 't1')
+df = pd.merge(df, results, how='left', left_on='ID #', right_on='id')
+df.drop('id', axis=1, inplace=True)
+results = []
+results = score.ders(ders_df_t2, results, '#_', 't2')
+df = pd.merge(df, results, how='left', left_on='ID #', right_on='id')
+df.drop('id', axis=1, inplace=True)
+results = []
+results = score.ders(ders_df_t3, results, '#_', 't3')
+df = pd.merge(df, results, how='left', left_on='ID #', right_on='id')
+df.drop('id', axis=1, inplace=True)
+
 
 # Make the PCRI DataFrames
 pcri_df_t1 = ['ID #']
@@ -90,56 +197,7 @@ df = pd.merge(df, results, how='left', left_on='ID #', right_on='id')
 df.drop('id', axis=1, inplace=True)
 
 
-# Make the CPS-AIM DataFrames
-cps_aim_t1 = ['ID #']
-cps_aim_t2 = ['ID #']
-cps_aim_t3 = ['ID #']
-
-df_time_columns('T1_TKCO', cps_aim_t1)
-df_time_columns('T2_TKCO', cps_aim_t2)
-df_time_columns('T3_TKCO', cps_aim_t3)
-
-cps_aim_df_t1 = df[cps_aim_t1]
-column_names = []
-split_dfs(cps_aim_df_t1, 3, 8)
-cps_aim_df_t1.columns = column_names
-
-cps_aim_df_t2 = df[cps_aim_t2]
-column_names = []
-split_dfs(cps_aim_df_t2, 3, 8)
-cps_aim_df_t2.columns = column_names
-
-cps_aim_df_t3 = df[cps_aim_t3]
-column_names = []
-split_dfs(cps_aim_df_t3, 3, 8)
-cps_aim_df_t3.columns = column_names
-
-cps_aim_t1.remove('ID #')
-cps_aim_t2.remove('ID #')
-cps_aim_t3.remove('ID #')
-all_cps_aim = []
-all_cps_aim.append(cps_aim_t1)
-all_cps_aim.append(cps_aim_t2)
-all_cps_aim.append(cps_aim_t3)
-for i in all_cps_aim:
-    df.drop(i, axis=1, inplace=True)
-
-
-results = []
-results = score.cps_aim(cps_aim_df_t1, results, '#_', 't1')
-df = pd.merge(df, results, how='left', left_on='ID #', right_on='id')
-df.drop('id', axis=1, inplace=True)
-results = []
-results = score.cps_aim(cps_aim_df_t2, results, '#_', 't2')
-df = pd.merge(df, results, how='left', left_on='ID #', right_on='id')
-df.drop('id', axis=1, inplace=True)
-results = []
-results = score.cps_aim(cps_aim_df_t3, results, '#_', 't3')
-df = pd.merge(df, results, how='left', left_on='ID #', right_on='id')
-df.drop('id', axis=1, inplace=True)
-
-# Adding the CPS-AIM
-
+# Adding simple statistics to the DataFrame
 df2 = round(df.mean(), 2)
 
 df2 = pd.DataFrame(df2)
